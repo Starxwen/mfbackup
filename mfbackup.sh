97c1e9f7adf5c381f1f00d4c192e5e7838adf2fe
星禾 <954204979@qq.com>
2023-11-24 02:02:47 +0000

    文件不备份本地，直接备份到远程服务器，减少本地硬盘使用
    Signed-off-by: 星禾 <954204979@qq.com>

--- mfbackup.sh
+++ mfbackup.sh
@@ -4,7 +4,7 @@ echo "魔方备份脚本"
 echo "作者：星跃云"
 
 if [ "$#" -ne 5 ]; then
-    echo "用法: bash <(curl -sS http://download.leapteam.cn/mfbackup.sh) <远程IP> <SSH用户名> <SSH密码> <最大文件大小> <备份目录>"
+    echo "用法: bash <(curl -sS http://download.leapteam.cn/mfbackup.sh) <远程IP> <SSH用户名> <SSH密码> <最大文件大小> <远程备份目录>"
     exit 1
 fi
 
@@ -12,10 +12,9 @@ REMOTE_SERVER_IP="$1"
 SSH_USERNAME="$2"
 SSH_PASSWORD="$3"
 MAX_SIZE="$4"
-BACKUP_DIR="$5"
+REMOTE_BACKUP_DIR="$5"
 SOURCE_DIR="/home/kvm"
 EXCLUDE_DIR="/home/kvm/images"
-mkdir -p "$BACKUP_DIR"
 
 # Check if sshpass is installed, if not, try to install it
 if ! command -v sshpass &> /dev/null; then
@@ -29,13 +28,15 @@ if ! command -v sshpass &> /dev/null; then
     echo "sshpass installed successfully."
 fi
 
-# Use rsync with --relative option to preserve the directory structure
-rsync -a --relative --exclude="$EXCLUDE_DIR" --max-size="$MAX_SIZE" "$SOURCE_DIR/" "$BACKUP_DIR"
+# Use find to get a list of files smaller than the specified size (excluding the specified directories)
+find "$SOURCE_DIR" -type f \( -not -path "$EXCLUDE_DIR/*" \) -size -"$MAX_SIZE" |
+while IFS= read -r file; do
+  # Adjust the source path to remove /home/kvm
+  relative_path="${file#$SOURCE_DIR/}"
 
-# Use sshpass to provide the password for ssh
-sshpass -p "$SSH_PASSWORD" rsync -a --progress "$BACKUP_DIR/" "$SSH_USERNAME@$REMOTE_SERVER_IP:$BACKUP_DIR/"
-
-# 删除本地备份目录
-rm -rf "$BACKUP_DIR"
+  echo "正在备份文件: $file"
+  sshpass -p "$SSH_PASSWORD" rsync -a --progress --rsync-path="mkdir -p \"$REMOTE_BACKUP_DIR/\$(dirname $relative_path)\" && rsync" "$file" "$SSH_USERNAME@$REMOTE_SERVER_IP:$REMOTE_BACKUP_DIR/$relative_path"
+  echo "备份完成: $file"
+done
 
 echo "备份完成."
